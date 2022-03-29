23eec744e4677dae2ebdae60a35357333974f770
jellas <jellasogno@gmail.com>
2022-03-30 04:23:20 +0900

    project

---
+++ consoleProject/src/project/Main.java
@@ -0,0 +1,12 @@
+package project;
+
+public class Main {
+
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) {
+		Behind app = new Behind();
+		
+		app.category();
+	}
+
+}
